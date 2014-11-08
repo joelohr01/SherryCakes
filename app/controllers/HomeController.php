96b42cf5a37a76169aaf380909297bcd0b2c895a
Joe <joe@joelohrdesign.com>
2014-11-08 08:50:33 -0600

    Controller Update

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,7 +7,33 @@ class HomeController extends BaseController {
     }
 
     public function doLogin() {
+        // validate the info
+        $rules = array(
+            'email'    => 'required|email',
+            'password' => 'required|alphaNum|min:3'
+        );
 
+        //run the validation
+        $validator = Validator::make(Input::all(), $rules);
+
+        //if validator fails redirect back to form
+        if ($validator->fails()) {
+            return Redirect::to('login')
+                ->withErrors($validator)
+                ->withInput(Input::except('password'));
+        } else {
+            $userdata = array(
+               'email' 	=> Input::get('email'),
+               'password' 	=> Input::get('password')
+            );
+
+            //attempt to do the login
+            if (Auth::attempt($userdata)) {
+                echo 'Success!';
+            } else {
+                return Redirect::to('login');
+            }
+        }
     }
 
 }
